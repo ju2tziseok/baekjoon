5286816d6df48377a2a875b70d392719c4c4f38a
Administrator <althtjr1127@naver.com>
2024-03-04 23:32:00 +0900

    2024-03-04

---
+++ Python/baekjoon/피보나치 수5 10870.py
@@ -0,0 +1,15 @@
+memo={}
+def f(n) :
+    if n in memo :
+        return memo[n]
+    if n==0 :
+        return 0
+    if n==1 :
+        return 1
+    else:
+        temp=(f(n-1)+f(n-2))
+        memo[n]=temp
+        return temp
+
+n=int(input())
+print(f(n))
