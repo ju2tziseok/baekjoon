66bd88bb1b4d8e2e1b0f65847180d8496869b90a
Administrator <althtjr1127@naver.com>
2025-01-09 22:59:45 +0900

    2025-01-09

---
+++ Python/baekjoon/알고리즘 수업 - 너비 우선 탐색.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n,m,v=map(int,input().split())
+graph=[[] for _ in range(n+1)]
+visited=[0]*(n+1)
+
+cnt=1
+
+for i in range(m):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for g in graph :
+    g.sort()
+
+def bfs(graph,v,visited) :
+    global cnt
+    queue=deque()
+    queue.append(v)
+    visited[v]=cnt
+    while queue :
+        v=queue.popleft()
+        for i in graph[v] :
+            if visited[i] == 0 :
+                cnt+=1
+                queue.append(i)
+                visited[i]=cnt
+
+bfs(graph,v,visited)
+
+
+for i in range(1,n+1) :
+    print(visited[i],end=' ')
