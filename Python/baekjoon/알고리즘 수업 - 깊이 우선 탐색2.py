535975d9528bda0314c59748d267977db63b3221
Administrator <althtjr1127@naver.com>
2025-01-09 23:06:49 +0900

    2025-01-09

---
+++ Python/baekjoon/알고리즘 수업 - 깊이 우선 탐색2.py
@@ -0,0 +1,33 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n,m,v=map(int,input().split())
+graph=[[] for _ in range(n+1)]
+visited=[0] * (n+1)
+cnt=1
+
+for _ in range(m) :
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for g in graph :
+    g.sort(reverse=True)
+
+
+
+
+def dfs(graph,v,visited) :
+    global cnt
+    visited[v]=cnt
+
+    for i in graph[v] :
+        if visited[i] == 0 :
+            cnt+=1
+            dfs(graph,i,visited)
+
+dfs(graph,v,visited)
+
+for i in range(1,n+1) :
+    print(visited[i])
