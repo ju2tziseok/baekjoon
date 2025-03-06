211b3480acfa0efaf3af2527a24e30011a58e0b7
Administrator <althtjr1127@naver.com>
2025-03-06 15:35:54 +0900

    2025-03-06

--- Python/baekjoon/도시 분할 계획.py
+++ Python/baekjoon/도시 분할 계획.py
@@ -1,34 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-def find_parent(parent, x) :
+def find_parent(parent,x) :
     if parent[x] != x :
         parent[x] = find_parent(parent,parent[x])
     return parent[x]
 
 def union_parent(parent,a,b) :
-    a= find_parent(parent,a)
-    b= find_parent(parent,b)
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
 
-    if a < b:
+    if a<b :
         parent[b] = a
     else :
         parent[a] = b
 
-
-n,m = map(int,input().split()) # n은 노드 m은 간선
+n,m = map(int,input().split())
 parent = [0] * (n+1)
 edges=[]
+result = 0
 
 for i in range(1,n+1) :
-    parent[i] = i
+    parent[i]= i
 
-for i in range(m) :
+for _ in range(m) :
     a,b,cost=map(int,input().split())
     edges.append((cost,a,b))
 
 edges.sort()
-result = 0
 
 for edge in edges :
     cost,a,b = edge
