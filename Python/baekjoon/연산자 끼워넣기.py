703710b30bdea658280cfd2f80ba087516c21121
Administrator <althtjr1127@naver.com>
2025-03-20 00:06:06 +0900

    2025-03-20

--- Python/baekjoon/연산자 끼워넣기.py
+++ Python/baekjoon/연산자 끼워넣기.py
@@ -1,37 +1,31 @@
-n=int(input())
-data=list(map(int,input().split()))
-add,sub,mul,div = map(int,input().split())
+n = int(input())
+data = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
 
-min_value = 1e9
-max_value = -1e9
-
-def dfs(i,now) :
-    global min_value, max_value, add ,sub, mul, div
-    if i == n :
-        min_value = min(min_value,now)
-        max_value = max(max_value,now)
-    else :
-        if add > 0 :
-            add-=1
-            dfs(i+1,now+data[i])
-            add+=1
-        if sub > 0 :
-            sub-=1
-            dfs(i+1, now-data[i])
-            sub+=1
-        if mul > 0 :
-            mul-=1
-            dfs(i+1, now *data[i])
-            mul+=1
-        if div > 0 :
-            div-=1
-            dfs(i + 1, now // data[i] if now >= 0 else -(-now // data[i]))
-            div+=1
+min_value = int(1e9)
+max_value = int(-1e9)
 
 
+def dfs(i, now, add, sub, mul, div):
+    global min_value, max_value
 
-dfs(1,data[0])
+    if i == n:
+        min_value = min(min_value, now)
+        max_value = max(max_value, now)
+        return
 
+    if add > 0:
+        dfs(i + 1, now + data[i], add - 1, sub, mul, div)
+    if sub > 0:
+        dfs(i + 1, now - data[i], add, sub - 1, mul, div)
+    if mul > 0:
+        dfs(i + 1, now * data[i], add, sub, mul - 1, div)
+    if div > 0:
+        dfs(i + 1, now // data[i] if now >= 0 else -(-now // data[i]), add, sub, mul, div - 1)
+
+
+# DFS 실행 (연산자 개수를 매개변수로 전달)
+dfs(1, data[0], add, sub, mul, div)
 
 print(max_value)
 print(min_value)
