8070875d0f18ca51cdc4a9cb51c99692ec3013a5
Administrator <althtjr1127@naver.com>
2025-04-28 17:30:05 +0900

    2025-03-25

---
+++ Python/baekjoon/정수 삼각형.py
@@ -0,0 +1,14 @@
+n=int(input())
+dp=[]
+for i in range(n):
+    dp.append(list(map(int,input().split())))
+
+for i in range(1,n):
+    for j in range(0,i+1) :
+        if j == 0 :
+            dp[i][0] += dp[i-1][0]
+        elif i == j :
+            dp[i][j] += dp[i-1][j-1]
+        else :
+            dp[i][j] += max(dp[i-1][j],dp[i-1][j-1])
+print(max(dp[n-1]))
